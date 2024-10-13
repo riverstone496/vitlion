3f34cb61d3ddf970731620c58afdd2a12a75a797
riverstone496 <robosatoki@outlook.jp>
2024-10-13 17:37:44 +0900

    add loss scalar

--- pretrain.py
+++ pretrain.py
@@ -364,12 +364,12 @@ def train_one_epoch(
             losses_m.update(loss.item(), input.size(0))
 
         optimizer.zero_grad()
-        # if loss_scaler is not None:
-        #     loss_scaler(
-        #         loss, optimizer,
-        #         clip_grad=args.clip_grad, clip_mode=args.clip_mode,
-        #         parameters=model_parameters(model, exclude_head='agc' in args.clip_mode),
-        #         create_graph=second_order)
+        if loss_scaler is not None:
+            loss_scaler(
+                loss, optimizer,
+                clip_grad=args.clip_grad, clip_mode=args.clip_mode,
+                parameters=model_parameters(model, exclude_head='agc' in args.clip_mode),
+                create_graph=second_order)
         if loss_scaler is None:
             loss.backward(create_graph=second_order)
             if args.clip_grad is not None:
